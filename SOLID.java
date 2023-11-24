662f3020d3a247fc2317b92e545f518408fed4e9
Suares <148288151+SuaresDe@users.noreply.github.com>
2023-11-24 16:02:10 -0300

    Add files via upload

---
+++ SOLID.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solid;
+
+/**
+ *
+ * O que é SOLID?
+ * 
+ * SOLID é um termo/acrônimo de 5 princípios da orienttação a objetos
+ * 
+ * 1* S - Princípio da Responsabilidade Única (Single Responsibility Principle - SRP):
+ * 
+ * Uma classe deve ter apenas uma razão para mudar, ou seja, deve ter apenas uma responsabilidade.
+ * 
+ * 2* O - Princípio do Aberto/Fechado (Open/Closed Principle - OCP):
+ * 
+ * Uma classe deve estar aberta para extensão, mas fechada para modificação. Isso significa que você deve ser capaz de estender o comportamento de uma classe sem alterar seu código fonte.
+ * 
+ * L - Princípio da Substituição de Liskov (Liskov Substitution Principle - LSP):
+ * 
+ * Objetos de uma classe derivada devem poder substituir objetos de uma classe base sem afetar a corretude do programa. Isso garante que uma subclasse possa ser usada em qualquer lugar onde sua classe base é usada.
+ * 
+ * I - Princípio da Segregação de Interfaces (Interface Segregation Principle - ISP):
+ * 
+ * Uma classe não deve ser forçada a implementar interfaces que ela não utiliza. Em outras palavras, uma classe não deve ser obrigada a ter métodos que não fazem sentido para ela.
+ * 
+ * D - Princípio da Inversão de Dependência (Dependency Inversion Principle - DIP):
+ * 
+ * Módulos de alto nível não devem depender de módulos de baixo nível. Ambos devem depender de abstrações. Além disso, abstrações não devem depender de detalhes, mas detalhes devem depender de abstrações. Esse princípio incentiva o uso de interfaces ou classes abstratas para desacoplar componentes do sistema.
+ * 
+ * 
+ * 
+ */
+public class SOLID {
+    System.out.println("BE SOLID!");
+}
